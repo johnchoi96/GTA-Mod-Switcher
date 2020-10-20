065254a6f55300eeff4382ed14ffc1d41d3e6529
John Choi <johnchoi1003@icloud.com>
2020-10-20 17:30:02 -0400

    Adding script

---
+++ gtamod.bat
@@ -0,0 +1,28 @@
+
+@echo off
+D:
+cd Steam\steamapps\common\
+
+ECHO Do you want to play with mods or just online?
+SET /P _userPrompt= Type either "mods" or "online": 
+
+ECHO %_userPrompt%
+IF "%_userPrompt%"=="mods" (
+
+	ECHO ENABLING MODS...
+	ren "Grand Theft Auto V" "Grand Theft Auto V - clean"
+	ren "Grand Theft Auto V - mods" "Grand Theft Auto V"
+
+) ELSE (
+
+	ECHO DISABLING MODS...
+	ren "Grand Theft Auto V" "Grand Theft Auto V - mods"
+	ren "Grand Theft Auto V - clean" "Grand Theft Auto V"
+
+)
+
+:end
+
+ECHO Task finished...
+SET /P _confirm= Press ENTER to finish...
+
