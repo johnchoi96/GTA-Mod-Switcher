1ab658c20db5a8575d913d7fc9854c83e8bc84cf
John Choi <johnchoi1003@icloud.com>
2020-12-24 01:02:10 -0500

    In progress of GUI build

---
+++ GTA_Launcher/GTA_Launcher/MainScreen.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace GTA_Launcher
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.ModToggleBtn = new System.Windows.Forms.Button();
+            this.RunGameBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.ModStatusLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ModToggleBtn
+            // 
+            this.ModToggleBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ModToggleBtn.Location = new System.Drawing.Point(79, 41);
+            this.ModToggleBtn.Name = "ModToggleBtn";
+            this.ModToggleBtn.Size = new System.Drawing.Size(75, 25);
+            this.ModToggleBtn.TabIndex = 0;
+            this.ModToggleBtn.Text = "Enable Mod";
+            this.ModToggleBtn.UseVisualStyleBackColor = true;
+            this.ModToggleBtn.Click += new System.EventHandler(this.ToggleClicked);
+            // 
+            // RunGameBtn
+            // 
+            this.RunGameBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RunGameBtn.Location = new System.Drawing.Point(79, 70);
+            this.RunGameBtn.Name = "RunGameBtn";
+            this.RunGameBtn.Size = new System.Drawing.Size(75, 25);
+            this.RunGameBtn.TabIndex = 1;
+            this.RunGameBtn.Text = "Run Game";
+            this.RunGameBtn.UseVisualStyleBackColor = true;
+            this.RunGameBtn.Click += new System.EventHandler(this.RunGameClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mod Status:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(64, 20);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(24, 13);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Idle";
+            // 
+            // ModStatusLabel
+            // 
+            this.ModStatusLabel.AutoSize = true;
+            this.ModStatusLabel.Location = new System.Drawing.Point(89, 37);
+            this.ModStatusLabel.Name = "ModStatusLabel";
+            this.ModStatusLabel.Size = new System.Drawing.Size(46, 13);
+            this.ModStatusLabel.TabIndex = 6;
+            this.ModStatusLabel.Text = "Enabled";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.StatusLabel);
+            this.groupBox1.Controls.Add(this.ModStatusLabel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 65);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Statuses";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ModToggleBtn);
+            this.groupBox2.Controls.Add(this.RunGameBtn);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(232, 132);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controls";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 28);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(238, 230);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(262, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(54, 22);
+            this.toolStripButton1.Text = "Set Path";
+            this.toolStripButton1.Click += new System.EventHandler(this.SetPathClicked);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 270);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainScreen";
+            this.Text = "GTA Launcher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ModToggleBtn;
+        private System.Windows.Forms.Button RunGameBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label ModStatusLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ GTA_Launcher/GTA_Launcher/MainScreen.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTA_Launcher
+{
+    public partial class MainScreen : Form
+    {
+
+        private string GamePath
+        {
+            get
+            {
+                return Properties.Settings.Default.GTAPath;
+            }
+            set
+            {
+                string[] delimited = value.Split('\\');
+                string directory = "";
+                for (int i = 0; i < delimited.Length - 1; i++)
+                {
+                    directory += delimited[i] + @"\";
+                }
+                Properties.Settings.Default.GTAPath = directory;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private bool GTAExists
+        {
+            get
+            {
+                string path = @"D:Steam\steamapps\common\Grand Theft Auto V";
+                path = Environment.ExpandEnvironmentVariables(path);
+                return Directory.Exists(path);
+            }
+        }
+
+        private bool ModEnabled
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        private bool ModExists
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public MainScreen()
+        {
+            InitializeComponent();
+            // check if game directory has been set
+            if (GamePath == "")
+            {
+                SetButtonState();
+            }
+        }
+
+        private void SetPathClicked(object sender, EventArgs e)
+        {
+            // if not set, let the user choose
+            string folderPath = "";
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                string title = "Directory not set!";
+                string message = "You must set the path of your GTA Game";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            folderPath = dialog.SelectedPath;
+            GamePath = folderPath;
+
+            SetButtonState();
+        }
+
+        private void ToggleClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine(Properties.Settings.Default.GTAPath == "");
+        }
+
+        private void RunGameClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine(ModExists);
+        }
+
+        private void SetButtonState()
+        {
+            ModToggleBtn.Enabled = GamePath != "";
+            RunGameBtn.Enabled = GamePath != "";
+            
+            if (GamePath == "")
+            {
+                ModStatusLabel.Text = "Game Path not Set";
+            } 
+            else
+            {
+                if (ModEnabled)
+                {
+                    ModStatusLabel.Text = "Enabled";
+                    ModToggleBtn.Text = "Disable Mod";
+                }
+                else
+                {
+                    ModStatusLabel.Text = "Disabled";
+                    ModToggleBtn.Text = "Enable Mod";
+                }
+            }
+        }
+    }
+}
